e96aebd6257e13cc8344716215aa62ed9b7b48af
TheMelonnn <zidanrifardi125@gmail.com>
2023-05-24 07:31:21 +0700

    Latihan Modul 5

---
+++ src/Latihan1/Main.java
@@ -0,0 +1,22 @@
+package Latihan1;
+
+public class Main {
+public static void main(String[] args) {
+	BinaryTree pohon = new BinaryTree();
+	
+	pohon.NewNode(20);
+	pohon.NewNode(2);
+	pohon.NewNode(25);
+	pohon.NewNode(37);
+	pohon.NewNode(16);
+	
+	System.out.println("Pre Order : ");
+	pohon.preOrder(pohon.root);
+	
+	System.out.println("In Order : ");
+	pohon.inOrder(pohon.root);
+	
+	System.out.println("Post Order : ");
+	pohon.postOrder(pohon.root);
+}
+}
